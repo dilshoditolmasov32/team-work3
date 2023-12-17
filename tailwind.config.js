488350186f23b898220e56fa55733baa3740b182
Dilshodbek20040104 <dilshoditolmasov32@gmail.com>
2023-12-17 09:25:00 +0500

    dilshodbek added

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mode: "jit",
+  content: ["./*.html"],
+  theme: {
+    extend: {},
+    colors: {
+      ColorFirst: "#525560",
+      ColorSecond: "#1D2130",
+    },
+  },
+
+  plugins: [],
+};
